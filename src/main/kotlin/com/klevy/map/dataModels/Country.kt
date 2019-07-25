0aaf8dac82c64634f11833e72bf7e86d360c0116
katielevy1 <katie.levy1@gmail.com>
2019-07-24 22:55:42 -0700

    Adding to country object

--- src/main/kotlin/com/klevy/map/dataModels/Country.kt
+++ src/main/kotlin/com/klevy/map/dataModels/Country.kt
@@ -3,7 +3,8 @@ package com.klevy.map.dataModels
 import javax.persistence.*
 
 @Entity
-data class Country(@Column(unique=true) val name: String, val population: Long = 0) {
+data class Country(@Column(unique=true) val name: String, val population: Long = 0, val government: String = "",
+                   val currency: String = "", val gdp: Long = 0, val other: String = "") {
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     val id: Long = 0
